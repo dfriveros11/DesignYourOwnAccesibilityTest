53a9574126377d06f8a2487c1e4b27c639f21a16
MateoLeon44 <30735098+MateoLeon44@users.noreply.github.com>
2020-05-11 23:56:02 -0500

    Uso de  mongoose

--- models/user-model.js
+++ models/user-model.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//Se puede utilizar passport sin tener que utilizar mongoose ya que no está permitido para la clase sadly :(
+
 const userSchema = new Schema({
   username: String,
   googleId: String,
